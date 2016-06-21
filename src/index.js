af10466dbc99936f3059196908aff289c293eeaa
Aliou Maiga <alioumaiga@alious-mbp.attlocal.net>
2016-06-21 02:09:56 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import SearchBar fom "./components/search_bar";
+
+const API_KEY ="AAIzaSyDX7G2xImneHuF8BTC58C_oolCPkkMGzPM";
+// Create a new component. This component should produce some HTML
+
+const App = () => {
+  return <div>
+<SearchBar />
+  </div>;
+}
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector(".container"));
