00d2db23c7ddf01879b627273f33397e8da8a9f4
Aliou Maiga <alioumaiga@alious-mbp.attlocal.net>
2016-06-30 09:53:29 -0700

    Added video search

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 
 const API_KEY ="AAIzaSyDX7G2xImneHuF8BTC58C_oolCPkkMGzPM";
+
+YTSearch({key:API_KEY, term:"surfboards"}, function (data){
+  console.log(data)
+});
 // Create a new component. This component should produce some HTML
 
 const App = () => {
